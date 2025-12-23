9eeb813d109263fb85a0727ad6a7475407771b11
Joseph Emefieh <chukby@gmail.com>
2025-12-23 02:01:06 +0100

    feat: added Rating model and serializer

---
+++ apps/ratings/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from apps.ratings.models import Rating
+
+class RatingSerializer(serializers.ModelSerializer):
+    rater = serializers.SerializerMethodField(read_only=True)
+    agent = serializers.SerializerMethodField(read_only=True)
+    
+    class Meta:
+        model = Rating
+        exclude = ["updated_at", "pkid"]
+    
+    def get_rater(self, obj):
+        return str(obj.rater.username)
+    
+    def get_agent(self, obj):
+        return str(obj.agent.user.username)
